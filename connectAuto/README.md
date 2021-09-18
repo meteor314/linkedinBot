ccd37fa3626d0a3677afff415d8d07bf8d3f35e8
root <root@pop-os.localdomain>
2021-09-18 20:02:40 +0200

    Fiexd some bugs need to Improved

--- connectAuto/README.md
+++ connectAuto/README.md
@@ -1,1 +1,19 @@
-# linkedin_bot
+# ConnectAuto
+With thi BOt you can simply connect send a connect Button Two all your second third or anyone else you want. 
+You just need to change the variable named "link". 
+You also need to define your variable path from Chrome and the chromedriver 
+(note that I actually use version 93 of chrome, but you will replace with your actual version of chrome.)
+
+
+You can find your actual version of the Browser on `chrome:://version`
+You also need to define Your default user path, 
+I didn't  recommend you to use your default chrome profile. You can create a new profile,and put the variable
+path on main.py.
+Install selenium with pip3 (IDE Pycharm is very recommended !) :
+`pip3 install -r requirements.txt`
+
+
+**PS : I'm not responsible for what you're doing, it's a simple BOT for education. Be careful.
+**And that's all. Enjoy Guys !****
+
+
--- connectAuto/main.py
+++ connectAuto/main.py
@@ -7,7 +7,7 @@ import random
 #********** LOG IN *************
 options = Options()
 options.add_argument("--user-data-dir=/home/meteor314/.config/google-chrome/Profile 1") #you need to change the profile path (you cand find this on chrome://version/ )
-chrome_path = r"/home/meteor314/Desktop/linkedin_bot/chromedriver"
+chrome_path = r"/home/meteor314/Desktop/linkedin_bot/connectAuto/chromedriver"
 options.page_load_strategy = 'normal'
 
 driver = webdriver.Chrome(chrome_path, options=options)
@@ -18,17 +18,34 @@ time.sleep(2)
 
 #***************** ADD CONTACTS ***********************
 
-driver.get("https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page=10")
+driver.get("https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page=0")
 time.sleep(2)
 
 all_buttons = driver.find_elements_by_tag_name("button")
-connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+start = 0
+try :
+    while start <= 100  : #click on all the fast 100 page connection.
+        connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+        for btn in connect_buttons:
+            try:
+                driver.execute_script("arguments[0].click();", btn)
+                time.sleep(2)
+                send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
+                driver.execute_script("arguments[0].click();", send)
+                close = driver.find_element_by_xpath("//button[@aria-label='Dismiss']")
+                driver.execute_script("arguments[0].click();", close)
+                time.sleep(2)
 
-for btn in connect_buttons:
-    driver.execute_script("arguments[0].click();", btn)
-    time.sleep(2)
-    send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
-    driver.execute_script("arguments[0].click();", send)
-    close = driver.find_element_by_xpath("//button[@aria-label='Dismiss']")
-    driver.execute_script("arguments[0].click();", close)
-    time.sleep(2)
+
+
+            except Exception as e:
+                print(e)
+        start = start + 1
+        #time.sleep(10)
+        startToStr = str(start)  #convert it to string get page variable
+        url = '"https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page="' + startToStr
+
+
+        driver.get(url)
+except Exception as e :
+    print(e)
