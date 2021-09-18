69b59ea02591e21eaa3a5413ed939ce5e3ced2ff
= <=>
2021-09-19 00:50:05 +0200

    Click on each link

--- connectAuto/main.py
+++ connectAuto/main.py
@@ -2,8 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-import datetime
-import random
+from selenium.common.exceptions import InvalidSessionIdException
 #********** LOG IN *************
 options = Options()
 options.add_argument("--user-data-dir=/home/meteor314/.config/google-chrome/Profile 1") #you need to change the profile path (you cand find this on chrome://version/ )
@@ -18,34 +17,54 @@ time.sleep(2)
 
 #***************** ADD CONTACTS ***********************
 
-driver.get("https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page=0")
+driver.get("https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page=3")
 time.sleep(2)
 
 all_buttons = driver.find_elements_by_tag_name("button")
-start = 0
+i = 0
+lists_url = []
+while i <= 100 :
+    link = "https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH" \
+           "&page=" + str(i)
+    lists_url.append(link)
+    i = i +1
+#print(lists_url)
 try :
-    while start <= 100  : #click on all the fast 100 page connection.
-        connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
-        for btn in connect_buttons:
-            try:
-                driver.execute_script("arguments[0].click();", btn)
-                time.sleep(2)
-                send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
-                driver.execute_script("arguments[0].click();", send)
-                close = driver.find_element_by_xpath("//button[@aria-label='Dismiss']")
-                driver.execute_script("arguments[0].click();", close)
-                time.sleep(2)
+    for list_url in lists_url[2:] :
+        time.sleep(3)
+        try :
+            connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+            for btn in connect_buttons:
+                time.sleep(1)
+                try:
+                    driver.execute_script("arguments[0].click();", btn)
+                    time.sleep(2)
+                    send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
+                    driver.execute_script("arguments[0].click();", send)
+                    close = driver.find_element_by_xpath("//button[@aria-label='Dismiss']")
+                    driver.execute_script("arguments[0].click();", close)
+                    time.sleep(1)
+
+                except Exception as e:
+                    print(e)
+
+            js = 'window.scrollTo(0, document.documentElement.scrollHeight)'  # scroll to the bottom of the page.
+            driver.execute_script(js)
+            btnNext = driver.find_element_by_xpath("//button[@aria-label='Next']")
+            driver.execute_script("arguments[0].click();", btnNext)
+
+            """print(list_url)
+            driver.execute_script('window.open("{}", "_blank");'.format(list_url))
+            time.sleep(5)
+            driver.switch_to.window(driver.window_handles[-1])  # switch to the tab recently open """
+            #driver.close()
+            time.sleep(7)
+            #driver.get(lists_url)
+
+        except Exception as e :
+            print(e)
+        time.sleep(3)
 
 
-
-            except Exception as e:
-                print(e)
-        start = start + 1
-        #time.sleep(10)
-        startToStr = str(start)  #convert it to string get page variable
-        url = '"https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D&origin=FACETED_SEARCH&page="' + startToStr
-
-
-        driver.get(url)
 except Exception as e :
     print(e)
